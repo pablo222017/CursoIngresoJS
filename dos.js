d0f456f1cd42597c32355d0bd4233681e611eb00
pablo222017 <40808881+pablo222017@users.noreply.github.com>
2018-07-06 21:58:51 -0300

    Add files via upload

---
+++ dos.js
@@ -0,0 +1,7 @@
+function mostrar()
+{
+	var nombre
+	var localidad
+	nombre=prompt("Escriba su nombre");
+	localidad=prompt("Escriba su localidad");
+}
---
+++ jsEntradaSalida-6.js
@@ -0,0 +1,24 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt) y Sumarlos.
+mostrar el resulto por medio de "ALERT"*/
+function sumar()
+{
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+	resultado=66;
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroUno=parseInt(numeroUno);
+	
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroDos=parseInt(numeroDos);
+
+
+
+
+	resultado=numeroUno + numeroDos;
+
+	alert("el resultado es " + resultado);
+}
+
