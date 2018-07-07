9f19fce122f931252c9be2acf21b191adaed7a1d
pablo222017 <40808881+pablo222017@users.noreply.github.com>
2018-07-06 22:21:46 -0300

    Add files via upload

---
+++ uno.js
@@ -0,0 +1,19 @@
+
+function mostrar()
+{
+	var base;
+	var altura;
+	var superficie;
+	var perimetro;
+	/* 
+	SE INGRESAN 3 NUMEROS, SE MUESTRA LA SUMA DE LOS 3, EL PROMEDIO DE LOS 3, Y LA RESTA DE LOS 3 EN UN UNICO ALERT
+	18,6,(-6)
+	*/
+	var numeroUno
+	var numeroDos
+	var numeroTres
+	numeroUno=document.getElementById('FormIngreso').value;
+	numeroDos=document.getElementById('FormIngreso').value;
+	numeroTres=document.getElementById('FormIngreso').value;
+	
+}
