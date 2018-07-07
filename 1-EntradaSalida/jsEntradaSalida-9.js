2f85573274740726ab5ecc08ce2d76229c1f74fd
lalalala <alalala@gmail.com>
2018-07-07 12:11:01 -0300

    ejercicios bien hechos

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,31 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+    //Entradas: sueldo
+    //Proceso: aumentar el sueldo en 10%
+    //salida: sueldo con el aumento
+    /*var sueldo
+    var aumento
+    var sueldoConAumento
+    sueldo = document.getElementById("sueldo").value
+    sueldo = parseInt(sueldo);
+    aumento = sueldo*(10/100);
+    sueldoConAumento = sueldo + aumento;
+    document.getElementById("resultado").value=sueldoConAumento; */
 
-	
+    /* var sueldo
+    var aumento
+    var sueldoConAumento
+    sueldo = document.getElementById("sueldo").value
+    sueldo = parseInt(sueldo);
+    sueldoConAumento = sueldo * 1.1
+
+
+
+    
+    
+    
+    
+    
+    //alert(sueldo + 1000);
 }
