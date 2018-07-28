9907c5b960062a22dc13664413436fa9a3d06627
pablo222017 <40808881+pablo222017@users.noreply.github.com>
2018-07-28 10:44:14 -0300

    iteracion while
    ejercicio 10

--- jsIteraciones (10).js
+++ jsIteraciones (10).js
@@ -1,31 +1,44 @@
-Al presionar el botón pedir números hasta que el usuario quiera, mostrar:
-/*
-Los numeros para testear son: 0, 2, 4, -7, -3
-1-Suma de los negativos = -10
-2-Suma de los positivos = 6
-3-Cantidad de positivos = 2
-4-Cantidad de negativos = 2
-5-Cantidad de ceros = 1
-6-Cantidad de números pares = 3 
-7-Promedio de positivos = 3
-8-Promedios de negativos = 
-9-Diferencia entre positivos y negativos, (positvos-negativos).
 function mostrar()
-*/
 {
+	var numero;
+	var respuesta=true;
+	var acumNegativos=0
+	var acumPositivos=0	
 	var contador=0;
-	var respuesta="si";
-	var numeros;
+	var contNegativos=0;
+	var contPositivos=0;
+	var promed
 	//declarar contadores y variables 
-	numeros=promp("Ingrese un número");
-	numeros=parseInt(numeros);
-	
-	while (respuesta!="no")
+	while(respuesta==true)
 	{
-		
+		numero=prompt("Ingrese numero");
+		numero=parseInt(numero);
+		if (numero>0)
+		{
+			acumPositivos=acumPositivos+numero;
+			contPositivos=contPositivos+1; //contPositivos=contPositivos++;
+		}else
+		{
+			if (numero < 0)
+			{
+				acumNegativos=acumNegativos+numero;
+				contNegativos=contNegativos+1
+			}
+		}
 	}
 
 
 
 
-}//FIN DE LA FUNCIÓN
+}
+//FIN DE LA FUNCIÓNE
+/*enunciado:
+Al presionar el botón pedir números hasta que el usuario quiera, mostrar:
+1-Suma de los negativos.
+ 2-Suma de los positivos. 
+ 3-Cantidad de positivos. 
+ 4-Cantidad de negativos.
+ 5-Cantidad de ceros. 
+ 6-Cantidad de números pares.
+ 7-Promedio de positivos.
+ 8-Promedios de negativos. 9-Diferencia entre positivos y negativos, (positvos-negativos).*/
